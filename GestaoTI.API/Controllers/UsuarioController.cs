b2101babd1353fb60446d7505b983e96fc49487e
Samuel <samuel201927@outlook.com>
2026-02-11 02:36:25 -0300

    Novo app de gestao de TI

---
+++ GestaoTI.API/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoTI.API.Controllers
+{
+    [ApiController]
+    [Route("api/usuario")]
+    public class UsuarioController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet("perfil")]
+        public IActionResult Perfil()
+        {
+            return Ok("Você está autenticado 🔐");
+        }
+    }
+}
---
+++ GestaoTI.API/Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestaoTI.API.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Nome { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Email { get; set; }
+
+        [Required]
+        public string SenhaHash { get; set; }
+
+        public string Perfil { get; set; } = "User";
+
+        public bool Ativo { get; set; } = true;
+
+        public DateTime CriadoEm { get; set; } = DateTime.UtcNow;
+
+    }
+}
